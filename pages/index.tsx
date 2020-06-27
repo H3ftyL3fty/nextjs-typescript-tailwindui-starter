8a33c9363d8346c687b93119243e02b68ed225f6
Tyler <tyler@tkhatch.com>
2020-06-26 19:18:02 -0500

    readme

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ const HomePage: React.FC = () => {
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css"></link>
       </Head>
       <button type="button" className="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs leading-4 font-medium rounded text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150">
-        Button text
+        Button
       </button>
     </div>
   );
